ae07105c6b8a2f95523e97ac4341f2eb221528fa
PratyushV-l <pratyushvel@outlook.com>
2025-08-06 09:04:41 +0530

    Added Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "My Project",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
